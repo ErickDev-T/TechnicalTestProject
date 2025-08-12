1dc7335f69fa83161682115fcf44726ab6db9520
Rick2222 <erickytm2222@gmail.com>
2025-08-12 19:28:34 -0400

    API finished

--- ProjectAPIStore/Controllers/UserController.cs
+++ ProjectAPIStore/Controllers/UserController.cs
@@ -17,11 +17,79 @@ namespace ProjectAPIStore.Controllers
             _context = context;
         }
 
+
+        //get users
         [HttpGet("listed")]
         public async Task<ActionResult<List<Usuario>>> GetUsuarios()
         {
             var usuarios = await _context.Usuarios.ToListAsync();
             return Ok(usuarios); //200
         }
+        //edit users
+        [HttpPost("SaveUser")]
+        public async Task<ActionResult<Usuario>> SaveUser(Usuario usuario)
+        {
+
+            usuario.FechaCreacion = DateTime.Now;
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created, usuario);
+        }
+
+        //update user
+        [HttpPut("update/{id}")]
+        public async Task<ActionResult<Usuario>> UpdateUser(int id, Usuario usuario)
+        {
+            var updateUser = await _context.Usuarios.FindAsync(id);
+            //checking if the user exist 
+
+            if (updateUser == null)
+            {
+                return NotFound(); //404
+            }
+
+            updateUser.Nombres = usuario.Nombres;
+            updateUser.Apellidos = usuario.Apellidos;
+            updateUser.Correo = usuario.Correo;
+            updateUser.Username = usuario.Username;
+
+            await _context.SaveChangesAsync();
+            return Ok(updateUser);
+        }
+
+
+        //delete user
+        [HttpDelete("delete/{id}")]
+        public async Task<ActionResult> DeletedUser(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            //checking if the user exist 
+            if (usuario == null)
+            {
+                return NotFound(); //404
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        //search by id
+        [HttpGet("search/{id}")]
+        public async Task<ActionResult<Usuario>> searcById(int id)
+        {
+
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            //checking if the user exist 
+            if (usuario == null)
+            {
+                return NotFound(); //404
+            }
+
+            return Ok(usuario);
+        }
+
+
     }
 }
